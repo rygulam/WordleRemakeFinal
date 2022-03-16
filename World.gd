777226d2c2255cd76a4403e5a93a561a2b3a7a97
Rygulam <rygulam@bgsu.edu>
2022-03-16 13:26:32 -0400

    Added word list file and functionality from WordleRemake2
    Added word list, reads in list at start, and chooses a word from list as the goal word.

--- World.gd
+++ World.gd
@@ -12,15 +12,31 @@ var columns_max
 
 var word_guesses_max
 
+# path to file containing list of all valid 5-letter words i.e. chosen dictionary
+onready var word_list_file = 'res://Assets/sgb-words.txt'
+
+# contains list of all valid 5-letter words
+var word_list_array = []
+
+# goal word needed to be guessed to win the game
+var goal_word
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	# open sgb-words.txt
+	load_file(word_list_file)
+	word_list_array.sort()
+	# pick a word randomly and set it to a variable
+	# seeds the RNG based on current time
+	randomize()
+	goal_word = word_list_array[randi() % word_list_array.size()]
+	print('goal word is: ' + goal_word)
+	
 	# word guesses total
 	word_guesses_max = word_groups_node.get_child_count()
 	# letters per word
 	columns_max = word_groups_node.get_child(0).get_child_count()
-	
-	print(word_guesses_max)
-	print(columns_max)
 	
 	for child_num in word_guesses_max:
 		word_groups.append(word_groups_node.get_child(child_num))
@@ -48,3 +64,18 @@ func _on_VirtualKeyboard_word_submitted():
 	# Otherwise, end the game
 	else:
 		print('***** GAME OVER *****')
+
+# loads in word file line-by-line and creates an array of strings
+func load_file(file):
+	
+	var f = File.new()
+	f.open(file, File.READ)
+	var index = 1
+	# iterate through all lines until the end of file is reached
+	while not f.eof_reached():
+		var line = f.get_line()
+		# add word to array
+		word_list_array.append(line)
+		index += 1
+	f.close()
+	return
