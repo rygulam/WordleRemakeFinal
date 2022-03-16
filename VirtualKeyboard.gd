f3ab61afe6555dd2a33097c55a260e88c27f68f8
Rygulam <rygulam@bgsu.edu>
2022-03-16 15:40:30 -0400

    Style box change in code!!!

--- VirtualKeyboard.gd
+++ VirtualKeyboard.gd
@@ -4,12 +4,17 @@ export(ButtonGroup) var group
 
 onready var line_edit_node = get_node("/root/World/LineEdit")
 
+var green_pressed = load("res://Assets/ButtonStyles/green_style_pressed.tres")
+
 signal text_updated
 signal word_submitted
 
 func _ready():
 	for i in group.get_buttons():
 		i.connect('pressed', self, 'button_pressed')
+		# https://godotengine.org/qa/64697/tabcontainer-custom_styles-panel-stylebox-resource-script
+		# THIS WORKS!!!
+		i.set("custom_styles/pressed", green_pressed)
 
 func button_pressed():
 	if group.get_pressed_button().text != 'ENTER' and group.get_pressed_button().text != 'BACK':
